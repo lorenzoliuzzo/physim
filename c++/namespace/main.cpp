c8b8dd01cd398ef25b3e19d693b0f6a03ffaf043
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2022-07-14 18:32:30 +0200

    ok

---
+++ c++/namespace/main.cpp
@@ -0,0 +1,30 @@
+
+
+#include "units.h"
+
+// using namespace physics;
+
+int main() {
+ 
+    
+    // objects::mass m(15.4, 1.2, "kilo"), m2(12.4, 0.7, "milli"); 
+
+    // tools::position::coordinates coord({3., 4., 4.}, "milli");
+
+    // m.mass::print();
+    // m2.mass::print(); 
+
+    // coord.print(); 
+
+    // m.activate_field(); 
+    // m2.print_attraction();
+
+    // m2.add_attraction(m.attraction(coord)); 
+    // m2.print_attraction();
+
+    // tools::constants::constant G_const("G"); 
+    // G_const.print();
+
+
+    return 0; 
+}
